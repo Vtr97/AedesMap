6396b6fbe472e0f2510eabc417f6f0c4113218ff
Vitor Marques <vitormr97@live.com>
2025-03-20 21:19:36 -0300

    initial config

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css"
+
+const App: React.FC = () => {
+  return(
+  <div className="app">
+    <h1>Mapa de focos</h1>
+  </div>
+  );
+};
+export default App;
