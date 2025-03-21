87dbdb088fd27940e40e328f886ff5d9dfc019e3
Vitor Marques <vitormr97@live.com>
2025-03-20 21:33:23 -0300

    style: muda identação para ficar de acordo com o lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import "./App.css"
+import "./App.css";
 
 const App: React.FC = () => {
-  return(
-  <div className="app">
-    <h1>Mapa de focos</h1>
-  </div>
-  );
+    return(
+        <div className="app">
+            <h1>Mapa de focos</h1>
+        </div>
+    );
 };
 export default App;
