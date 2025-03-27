91575128b4513d987018b0f7b6d96d64d0cd46ce
Vitor Marques <vitormr97@live.com>
2025-03-26 21:11:09 -0300

    feat: adiciona funcionalidade de confirmar o reporte para o reportMenu

--- src/components/ReportMenu/ReportMenu.tsx
+++ src/components/ReportMenu/ReportMenu.tsx
@@ -1,33 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./ReportMenu.module.css";
 import Button from "../PageButton/Button";
 import { IoLocationOutline } from "react-icons/io5";
 import { FaPen } from "react-icons/fa";
+import { GiConfirmed } from "react-icons/gi";
 
-/* interface ReportMenuProps {
-    location: string;
-    description: string;
-} */
 const location = "rua teste 123 - Bairro Teste, Santo andre- SP";
 const description = "água parada embaixo de um pneu";
 
 const ReportMenu: React.FC = () => {
+    const [active, setActive] = useState(false);
+
+    const reportConfirm = () => {
+        return active ? setActive(false):setActive(true);
+    };
+
+    const Menu = () => {
+        if (active) {
+            return (
+                <div className={styles["report-menu"]}>
+                    <div className={styles.title}>
+                        <h2> Adicionar foco</h2>
+                    </div>
+                    <div className={styles.location}>
+                        <h3>
+                            {" "}
+                            <IoLocationOutline /> localização do foco:
+                        </h3>
+                        <p>{location}</p>
+                    </div>
+                    <div className={styles.description}>
+                        <h3>
+                            {" "}
+                            <FaPen /> Descrição do foco:
+                        </h3>
+                        <p>{description}</p>
+                    </div>
+
+                    <div className={styles.buttons}>
+                        <Button variant="emphasized" onClick={reportConfirm}>
+                            {" "}
+                            Salvar foco
+                        </Button>
+                    </div>
+                </div>
+            );
+        } else {
+            return (
+                <div className={styles.confirmation}>
+                    <div className={styles.message}>
+                        <h2>
+                            {" "}
+                            <GiConfirmed size={"5rem"}/>
+                            <br/>
+                            Foco adicionado com sucesso!
+                        </h2>
+                    </div>
+                    <div className={styles.buttons}>
+                        <Button variant="emphasized" onClick={reportConfirm}>
+                            {" "}
+                            OK
+                        </Button>
+                    </div>
+                </div>
+            );
+        }
+    };
+
     return (
-        <div className={styles["report-menu"]}>
-            <div className={styles.title}>
-                <h2> Adicionar foco</h2>
-            </div>
-            <div className={styles.location}>
-                <h3> <IoLocationOutline /> localização do foco:</h3>
-                <p>{location}</p>
-            </div>
-            <div className={styles.description}>
-                <h3> <FaPen/> Descrição do foco:</h3>
-                <p>{description}</p>
-            </div>
-            <div className={styles.buttons}>
-                <Button variant="emphasized"> Salvar foco</Button>
-            </div>
+        <div className={styles.wrapper}>
+            <Menu />
         </div>
     );
 };
